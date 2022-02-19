d8421c501a26caefb9488ac1f0727e9d30751ed2
Himanshu Masant <masanthimanshu@gmail.com>
2022-02-19 12:57:18 +0530

    Mobile Navigation Menu

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,11 @@ var swiper = new Swiper(".mySwiper", {
   effect: "cards",
   grabCursor: true,
 });
+
+$("#toggleView").click(() => {
+  $("#mobile-nav").toggleClass("none");
+});
+
+$("#toggleNav").click(() => {
+  $("#mobile-nav").toggleClass("none");
+});
